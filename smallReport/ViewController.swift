7d94e3190578be16a64635bdb5feb193e4a97ffe
user <user@userde-MacBook-Pro.local>
2017-10-09 15:56:30 +0800

    Initial Commit

---
+++ smallReport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smallReport
+//
+//  Created by user on 2017/10/9.
+//  Copyright © 2017年 tzuyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
